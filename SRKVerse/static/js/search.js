374f2f443547112a4eef5cf90b45ea022f1ee6c4
Trina <roy.trina2013@gmail.com>
2025-09-30 12:18:31 +0100

    Starting fresh, again

--- SRKVerse/static/js/search.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.querySelector('input[name="q"]');
-    searchInput.addEventListener('input', () => {
-        // Add AJAX call to API for live suggestions
-        console.log('Searching for:', searchInput.value);
-    });
-});
--- SRKVerse/static/js/utils.js
+++
@@ -1,5 +0,0 @@
-function loadContent(url) {
-    fetch(url).then(response => response.text()).then(html => {
-        document.querySelector('main').innerHTML = html;
-    });
-}
